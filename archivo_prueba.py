beb2d656ab09de59402c5d4acd6ca38fdd211d30
pipemayo01 <141164260+pipemayo01@users.noreply.github.com>
2024-06-11 17:19:01 -0400

    Add files via upload

---
+++ archivo_prueba.py
@@ -0,0 +1,13 @@
+lista_impar = []
+lista_par = []
+
+while num < 6000:
+    if num % 2:
+      lista_impar.append(num)
+    else:
+      lista_impar.append(num)
+    print(lista_impar)
+    print(lista_par)
+
+    
+
